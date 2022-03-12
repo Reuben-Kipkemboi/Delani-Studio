754ff492c066752e3d414daef0ca75ed4baa3e5c
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-03-13 00:15:14 +0300

    st:paragraph-styling - optimize paragraph styling

---
+++ JS/scripts.js
@@ -0,0 +1,9 @@
+ // design
+ $("#design-show").click(function() {
+    $("#design-show").hide();
+    $("#design-hide").toggle();
+});
+$("#design-hide").click(function() {
+$("#design-hide").hide();
+$("#design-show").toggle();
+});
