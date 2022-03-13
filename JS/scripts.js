03cf96d86511d761505881c62042e2b6cceccff5
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-03-13 17:41:08 +0300

    st:portfolio-section - create an hoover effect on porfolio projects

--- JS/scripts.js
+++ JS/scripts.js
@@ -34,7 +34,65 @@ $(document).ready(function () {
         $('#product').toggle();
     });
 
+    //the hover effect on portfolio images
 
+    $('#portfolio-img1').mouseover(function () {
+        $('#img-hover1').show()
+    })
+    $('#portfolio-img1').mouseout(function () {
+        $('#img-hover1').hide()
+    })
+
+    $('#portfolio-img2').mouseover(function () {
+        $('#img-hover2').show()
+    })
+    $('#portfolio-img2').mouseout(function () {
+        $('#img-hover2').hide()
+    })
+
+    $('#portfolio-img3').mouseover(function () {
+        $('#img-hover3').show()
+    })
+    $('#portfolio-img3').mouseout(function () {
+        $('#img-hover3').hide()
+    })
+
+    $('#portfolio-img4').mouseover(function () {
+        $('#img-hover4').show()
+    })
+    $('#portfolio-img4').mouseout(function () {
+        $('#img-hover4').hide()
+    })
+
+    $('#portfolio-img5').mouseover(function () {
+        $('#img-hover5').show()
+    })
+    $('#portfolio-img5').mouseout(function () {
+        $('#img-hover5').hide()
+    })
+    $('#portfolio-img6').mouseover(function () {
+        $('#img-hover6').show()
+    })
+    $('#portfolio-img6').mouseout(function () {
+        $('#img-hover6').hide()
+    })
+
+    $('#portfolio-img7').mouseover(function () {
+        $('#img-hover7').show()
+    })
+    $('#portfolio-img7').mouseout(function () {
+        $('#img-hover7').hide()
+    })
+
+    $('#portfolio-img8').mouseover(function () {
+        $('#img-hover8').show()
+    })
+    $('#portfolio-img8').mouseout(function () {
+        $('#img-hover8').hide()
+    })
+
+
+    // form validation .........................................................
     function validate() {
         let userName = document.getElementById('name');
         let userEmail = document.getElementById('email');
@@ -44,35 +102,26 @@ $(document).ready(function () {
             userName.value.length == 0 || userEmail.value.length == 0) {
             alert("UserName  and email cannot be empty")
             return false;
-        } else {
-            // alert('Good to go')
-        }
+        } else {}
         return true;
 
     }
-
-    function submitQuery(){
+    // Business Logic..........................................................
+    function submitQuery() {
         let userName = document.getElementById('name').value;
         let userEmail = document.getElementById('email');
         let query = document.getElementById('user-query');
-        if (userName){
+        if (userName) {
             alert("Hello" + " " + userName + " " + "we have received your message.Thankyou for reaching out to us");
-        }else{
+        } else {
             ('kindly check the format of email or your name');
         }
     }
-
+    // Event listener when the user clicks the submit button.
     let btn = document.getElementById('submit');
     btn.addEventListener('click', (event) => {
         event.preventDefault()
-
         validate();
-
         submitQuery();
-
-
-
-
-
     })
 });
