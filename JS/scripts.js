cc3954430c996ec1fe7d1197807d51ab3ad0be70
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-03-13 08:24:43 +0300

    st:paragraph-styling - validate toggle function

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,36 @@
- // design
- $("#design-show").click(function() {
-    $("#design-show").hide();
-    $("#design-hide").toggle();
-});
-$("#design-hide").click(function() {
-$("#design-hide").hide();
-$("#design-show").toggle();
-});
+// What we do section  on mouse click
+$(document).ready(function(){
+    // design toggle
+    $('#design1').click(function(){
+        $('#design-par').toggle();
+        $('#design1').toggle();
+    });
+
+    $("#design-par").click(function() {
+        $("#design-par").toggle();
+        $("#design1").toggle();
+    });
+
+        // Development-toggle
+
+    $('#development1').click(function(){
+        $('#development-par').toggle();
+        $('#development1').toggle();
+    });
+
+    $("#development-par").click(function() {
+        $("#development-par").toggle();
+        $("#development1").toggle();
+    });
+
+    // product management toggle
+    $('#product').click(function(){
+        $('#product-par').toggle();
+        $('#product').toggle();
+    });
+
+    $("#product-par").click(function() {
+        $("#product-par").toggle();
+        $("#product").toggle();
+    });
+})
